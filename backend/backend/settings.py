9dc2f9505952adaee58964f94dd252298ef4eb80
SEA <sinadavoudi84@gmail.com>
2025-07-09 19:56:44 +0330

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://asr-iran-front.vercel.app/"
+    "https://asr-iran-front.vercel.app"
 ]
 
 ALLOWED_HOSTS = ['*']
